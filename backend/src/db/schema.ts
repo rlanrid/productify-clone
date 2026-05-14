4bed08e5dae9fcb9b43937985911c3d701a38e72
rlan <rladnrider77@gmail.com>
2026-05-14 21:52:12 +0900

    스키마 및 릴레이션 정의

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,48 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+// Schema
+export const users = pgTable("users", {
+  id: text("id").primaryKey(),
+  email: text("email").notNull().unique(),
+  name: text("name"),
+  imageUrl: text("image_url"),
+  createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const products = pgTable("products", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  imageUrl: text("image_url").notNull(),
+  userId: text("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const comments = pgTable("comments", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  content: text("content").notNull(),
+  userId: text("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+  productId: text("user_id").notNull().references(() => products.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+});
+
+
+// Relations
+export const userRelations = relations(users, ({ many }) => ({
+  products: many(products), // 한 유저 -> 많은 상품
+  comments: many(comments), // 한 유저 -> 많은 댓글
+}));
+
+export const productRelations = relations(products, ({ one, many }) => ({
+  // 'fields' = 이 테이블의 외래키 (products.userId), 'references' = 해당 테이블의 기본 키 (users.id)
+  users: one(users, { fields: [products.userId], references: [users.id] }),
+  comments: many(comments),
+}));
+
+export const commentRelations = relations(comments, ({ one }) => ({
+  user: one(users, { fields: [comments.userId], references: [users.id] }),
+  product: one(products, { fields: [comments.productId], references: [products.id] });
+}));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware());
+app.use(express.json()); // JSON 파싱
+app.use(express.urlencoded({ extended: true })); // 데이터 파싱
 
 app.get("/", (req, res) => {
   res.json({
@@ -14,4 +21,4 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(ENV.PORT, () => console.log("서버 동작 중 - PORT:3000"));
+app.listen(ENV.PORT, () => console.log("서버 동작 중 - PORT:3000", ENV.PORT));
